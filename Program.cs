ba20ae51e76ea37970fff33bec544638215e40ba
alexandor09 <orzhekhovskiy-a@mail.ru>
2022-11-20 01:22:17 +0300

    https://www.codewars.com/kata/576b072359b1161a7b000a17/train/csharp

--- Program.cs
+++ Program.cs
@@ -1,40 +1,65 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 
-namespace CodeWars    // https://www.codewars.com/kata/5503013e34137eeeaa001648/train/csharp
+namespace CodeWars    // https://www.codewars.com/kata/576b072359b1161a7b000a17/train/csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var res = print(5);           
+            var res = GenerateDiagonal(3, 7);           
         }
-        public static string print(int n)
+        public static BigInteger[] GenerateDiagonal(int n, int l)
         {
-            if (n < 1 || n%2 == 0)
+            BigInteger[] result = new BigInteger[l];
+
+            for (int i = 0; i < l; i++)
             {
-                return null;
+                var a = Factorial(i, n + i);               
+                result[i] = a.Item3/(a.Item2*a.Item1);
             }
-            int heigth = n / 2 + 1;
 
-            StringBuilder stringBuilder = new StringBuilder();
-            int i = 0;
-            int sh = 1;
-            for (int a = 0; a < 2; a++)
+            return result;
+        }
+        public static (BigInteger, BigInteger , BigInteger) Factorial (int n, int k)
+        {
+            BigInteger bigInteger = new BigInteger();
+            BigInteger bigInteger2 = new BigInteger();
+            BigInteger bigInteger3 = new BigInteger();
+            bigInteger = 1;
+            bigInteger2 = 1;
+            int i = 1;
+            if (n > k - n)
             {
-                while ( i < heigth && i > -1)
+                for (; i <= k - n; i++)                
+                    bigInteger2 *= i;
+                
+                bigInteger = bigInteger2;
+            }
+            
+            for (; i <= n; i++)            
+                bigInteger *= i;
+            
+            if (n <= k - n)
+            {
+                bigInteger2 = bigInteger;
+                for (; i <= k - n; i++)
                 {
-                    stringBuilder.Append(String.Concat(Enumerable.Repeat(" ", heigth - i - 1)));
-                    stringBuilder.Append(String.Concat(Enumerable.Repeat("*", i * 2 + 1)));
-                    stringBuilder.Append("\n");
-                    i += sh;
+                    bigInteger2 *= i;
                 }
-                i -= 2;
-                sh = -1;
-            }   
+                bigInteger3 = bigInteger2;
+            }
 
-            return stringBuilder.ToString();            
+            else            
+                bigInteger3 = bigInteger;
+                      
+
+            for (; i <= k; i++)            
+                bigInteger3 *= i;
+            
+            return (bigInteger , bigInteger2 , bigInteger3);
         }
     }
 }
