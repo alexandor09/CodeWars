3fb4ba3171e3d04ad8b1d3a057d5c23be54edecf
alexandor09 <orzhekhovskiy-a@mail.ru>
2022-11-26 12:02:18 +0300

    https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/csharp

--- Program.cs
+++ Program.cs
@@ -3,63 +3,51 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 
-namespace CodeWars    // https://www.codewars.com/kata/576b072359b1161a7b000a17/train/csharp
+namespace CodeWars    // https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var res = GenerateDiagonal(3, 7);           
+            var a = Alphanumeric("Mazinkaiser");
+            Console.WriteLine(a);
         }
-        public static BigInteger[] GenerateDiagonal(int n, int l)
-        {
-            BigInteger[] result = new BigInteger[l];
 
-            for (int i = 0; i < l; i++)
-            {
-                var a = Factorial(i, n + i);               
-                result[i] = a.Item3/(a.Item2*a.Item1);
-            }
-
-            return result;
-        }
-        public static (BigInteger, BigInteger , BigInteger) Factorial (int n, int k)
+        public static bool Alphanumeric(string str)
         {
-            BigInteger bigInteger = new BigInteger();
-            BigInteger bigInteger2 = new BigInteger();
-            BigInteger bigInteger3 = new BigInteger();
-            bigInteger = 1;
-            bigInteger2 = 1;
-            int i = 1;
-            if (n > k - n)
+            if (str.Length == 0)
             {
-                for (; i <= k - n; i++)                
-                    bigInteger2 *= i;
-                
-                bigInteger = bigInteger2;
+                return false;
             }
-            
-            for (; i <= n; i++)            
-                bigInteger *= i;
-            
-            if (n <= k - n)
+            for (int i = 0; i < str.Length; i++)
             {
-                bigInteger2 = bigInteger;
-                for (; i <= k - n; i++)
+                if (IsCharOrDec(str[i]))
                 {
-                    bigInteger2 *= i;
+                    continue;
+                }              
+                return false;
+            }
+            return true;
+        }
+
+        static Predicate<char> IsCharOrDec = (char a) =>
+        {
+            int b = a;
+
+            if (b > 64 && b < 123)  // IsChar
+            {
+                if (b > 90 && b < 97)
+                {
+                    return false;
                 }
-                bigInteger3 = bigInteger2;
+                return true;
             }
 
-            else            
-                bigInteger3 = bigInteger;
-                      
-
-            for (; i <= k; i++)            
-                bigInteger3 *= i;
+            if (b > 47 && b < 58)  // is Numeric
+                return true;
             
-            return (bigInteger , bigInteger2 , bigInteger3);
-        }
+            return false;
+        };
+
     }
 }
