849f6664b0e59c84e2d530d5b0f4669b49ff50df
alexandor09 <orzhekhovskiy-a@mail.ru>
2022-12-04 00:23:25 +0300

    https://www.codewars.com/kata/57bf7fae3b3164dcac000352/train/csharp

--- Program.cs
+++ Program.cs
@@ -3,35 +3,62 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 
-namespace CodeWars    // https://www.codewars.com/kata/52774a314c2333f0a7000688/train/csharp
+namespace CodeWars    // https://www.codewars.com/kata/57bf7fae3b3164dcac000352/train/csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var a = ValidParentheses("()");
+            var a = FindPrimeSextuplet(600);
             Console.WriteLine("answer " + a);
             
         }
-        public static bool ValidParentheses(string input)
-        {            
-            int n = 0;
-            foreach (var item in input)
+        public static int[] FindPrimeSextuplet(int sumLimit)
+        {
+            int start = sumLimit / 6 - 8;
+            int[] arr = new int[6];
+            int sh = 0;
+
+            if (start % 2 == 0)
+                start++;
+
+            for (int i = start; i < int.MaxValue; i+=2)
             {
-                if (n < 0)               
-                    return false;
-               
-                if (item == '(')                
-                    n++;
-                
-                if (item == ')')                
-                    n--; 
+                if (FindSImple(i))
+                {                    
+                    arr[sh] = i;                    
+                    if (sh % 2 == 0)                    
+                        i += 2;
+                   
+                    sh++;
+
+                    if (arr[5] != 0)                    
+                        return arr;
+
+                    continue;
+                }
+                if (sh != 0)
+                {
+                    sh = 0;
+                    i = arr[sh];
+                }
             }
-
-            if (n == 0)
-                return true;
-
-            return false;
+            return arr;  
         }
+
+        public static bool FindSImple(int sumLimit)
+        {
+            int length = (int)Math.Sqrt(sumLimit) + 1;
+
+            for (int i = 3; i < length; i+=2)
+            {
+                if (sumLimit % i == 0)
+                {
+                    return false;
+                }                
+            }
+            return true;
+        }
+
     }
 }
