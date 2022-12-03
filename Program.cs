e4ca4019c9d672787679c46bbc7a61406ab82e8a
alexandor09 <orzhekhovskiy-a@mail.ru>
2022-12-03 19:39:13 +0300

    https://www.codewars.com/kata/52685f7382004e774f0001f7/train/csharp

--- Program.cs
+++ Program.cs
@@ -3,62 +3,33 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 
-namespace CodeWars    // https://www.codewars.com/kata/55f4e56315a375c1ed000159/train/csharp
+namespace CodeWars    // https://www.codewars.com/kata/52685f7382004e774f0001f7/train/csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var a = PowerSumDigTerm(23);
+            var a = GetReadableTime(359999);
             Console.WriteLine("answer " + a);
         }
-        public static long PowerSumDigTerm(int n)
+        public static string GetReadableTime(int seconds)
         {
-            int result = 4;            
-            int sh1 = 0;
-            long[] arr = new long[35];
-            while (sh1 <= 34)
+            if (seconds > 359999)            
+                return "99:59:59";
+
+            if (seconds >= 86400)
             {
-                for (int i = 2; i < 10; i++)
-                {
-                    if (IsEq(Math.Pow(result, i)))
-                    {
-                        Console.WriteLine($"FIND!!! {Math.Pow(result, i)}   {sh1}");
-                        arr[sh1] = (long)Math.Pow(result, i);
-                        sh1++;
-                    }
-                }
-                Console.WriteLine(result);
-                result++;
-                
+                int day = seconds / 86400;
+                seconds -= day * 86400;
+
+                var b = TimeSpan.FromSeconds(seconds);
+                var str = b.ToString();
+                int hour = int.Parse(str.Substring(0, 2)) + 24 * day;
+                str = hour.ToString() + str.Substring(2,str.Length-2);
+                return str;
             }
-            Array.Sort(arr);
-            var resArr = arr.Distinct().ToArray();            
-            return resArr[n - 1];
-           
-        }
-        public static bool IsEq (double x)
-        {
-            var a = x.ToString();
-            int res = 0;
-            long result = (long)x;
-            for (int i = 0; i < Math.Log10(x); i++)
-            {
-                res += (int)(result - (int)(result / 10) * 10);
-                result /= 10;
-            }           
-            if (res <=1)            
-                return false;
-            
-            result = 1L;
-
-            while (result < x)           
-                result *= res;
-            
-            if (result == x)
-                return true;
-
-            return false;
+            var c = TimeSpan.FromSeconds(seconds);
+            return c.ToString();
         }
     }
 }
