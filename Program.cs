2ae0d73d87aa87b82f6aba56a7fc31392f1a990f
alexandor09 <orzhekhovskiy-a@mail.ru>
2022-12-03 23:07:31 +0300

    https://www.codewars.com/kata/52774a314c2333f0a7000688/train/csharp

--- Program.cs
+++ Program.cs
@@ -3,37 +3,35 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 
-namespace CodeWars    // https://www.codewars.com/kata/5541f58a944b85ce6d00006a/train/csharp
+namespace CodeWars    // https://www.codewars.com/kata/52774a314c2333f0a7000688/train/csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var a = productFib(4895);
+            var a = ValidParentheses("()");
             Console.WriteLine("answer " + a);
-
-            foreach (var item in a)
-            {
-                Console.WriteLine(item);
-            }
-        }
-        public static ulong[] productFib(ulong prod)
-        {
-            ulong n1 = 0;
-            ulong n2 = 1;
-            while (n1 * n2 < prod)
-            {
-                ulong temp = n2;
-                n2 += n1;   
-                n1 = temp;
-            }
-
-            ulong[] arr = new ulong[] { n1, n2, 0 };
-
-            if (n1 * n2 == prod)            
-                arr[2] = 1;        
             
-            return arr;
-        }        
+        }
+        public static bool ValidParentheses(string input)
+        {            
+            int n = 0;
+            foreach (var item in input)
+            {
+                if (n < 0)               
+                    return false;
+               
+                if (item == '(')                
+                    n++;
+                
+                if (item == ')')                
+                    n--; 
+            }
+
+            if (n == 0)
+                return true;
+
+            return false;
+        }
     }
 }
