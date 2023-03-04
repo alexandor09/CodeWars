ef46f5b16d5587aa20db4a7c4d3b527960ca237f
alexandor09 <orzhekhovskiy-a@mail.ru>
2023-03-04 11:20:56 +0300

    chess horse move

--- Program.cs
+++ Program.cs
@@ -1,60 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Numerics;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace CodeWars    // https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4/train/csharp
+namespace Laba_1_Gourse
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var a = Gap(10, 1301, 1401);
-            Console.WriteLine("answer " + a);
-            foreach (var item in a)
+            int start_i = 6;    // стартовая точка по высоте
+            int start_j = 6;    // стартова точка по ширине
+            int fin_i = 7;      // финишная точка по высоте
+            int fin_j = 7;      // финишная точка по ширине
+
+            while ((Math.Abs(start_i + start_j - fin_i - fin_j) >= 3)) // пока расстояние больше 3, то подходить
             {
-                Console.WriteLine(item);
+                Podshag(ref start_i, ref start_j, fin_i, fin_j);
             }
-            
-        }
-        public static long[] Gap(int g, long m, long n)
-        {          
-            if (m % 2 == 0)
-                m++;
 
-            while (m + g <= n)
-            {    
-                if (IsSimple(m) && IsSimple(m + g))
-                {
-                    if (g == 2)
-                        return new long[] { m, (m + g) };
-                   
-
-                    for (int i = 2; i < g; i+=2)
-                    {
-                        if (IsSimple(m + i))
-                           break;
-                       
-                        else if (i + 2 == g)
-                            return new long[] { m, (m + g) };
-                    }
-                }
-                m+=2;
+            if (Win_or_Loos(start_i, start_j, fin_i, fin_j) || (start_i == fin_i && start_j == fin_j))
+            {
+                Console.WriteLine("You win!!");
+                return;
             }
-            return null;
+
+            if ((Math.Abs(start_i - fin_i) + (Math.Abs(start_j - fin_j)) == 2))
+            {
+                if (start_i == fin_i || start_j == fin_j)
+                    Rasstoianie_2_Vbok(ref start_i, ref start_j, fin_i, fin_j);
+                else
+                    Diagonal(ref start_i, ref start_j, fin_i, fin_j);
+            }
+            if ((Math.Abs(start_i - fin_i) + (Math.Abs(start_j - fin_j)) == 1))
+            {
+                Rasstoianie_1(ref start_i, ref start_j, fin_i, fin_j);
+            }
+
         }
 
-        public static bool IsSimple(long n)
+        public static void Podshag(ref int start_i, ref int start_j, int fin_i, int fin_j)  // подходим до тех пор, пока расстояние не будет равно менее 3
         {
-            double length = Math.Sqrt((double)n) + 1;
-            for (int i = 3; i < length; i+=2)
+            if (Math.Abs(start_i - fin_i) > Math.Abs(start_j - fin_j)) // по i больше
             {
-                if (n % i == 0)
-                    return false;
+                start_i = start_i > fin_i ? start_i - 2 : start_i + 2;
+                start_j = start_j > fin_j ? start_j - 1 : start_j + 1;
             }
-            return true;
+            else  // по j больше
+            {
+                start_i = start_i > fin_i ? start_i - 1 : start_i + 1;
+                start_j = start_j > fin_j ? start_j - 2 : start_j + 2;
+            }
+        }
+        public static void Diagonal(ref int start_i, ref int start_j, int fin_i, int fin_j)  // в худшем случае
+                                                                                             // на 6 ход мы придём к финишу
+        {
+            start_i = fin_i;
+            if (fin_j > 4)      // если он больше 4, то ход в другую сторону           
+                start_j = start_j - 2;
+
+            else
+                start_j = start_j + 2;    // первый ход
+
+            if (Math.Abs(start_i + start_j - fin_i - fin_j) == 1)
+            {
+                Rasstoianie_1(ref start_i, ref start_j, fin_i, fin_j);
+                Console.WriteLine("You Win!!!");
+                return;
+            }
+
+            start_i = start_i > 5 ? start_i - 2 : start_i + 2;
+            start_j = start_j > 6 ? start_j - 1 : start_j + 1;   // второй ход
+
+            start_i = start_i > 5 ? start_i - 1 : start_i + 1;
+            start_j = fin_j;                                        // третий ход
+
+            if (Math.Abs(start_i + start_j - fin_i - fin_j) == 1)
+            {
+                Rasstoianie_1(ref start_i, ref start_j, fin_i, fin_j);
+                Console.WriteLine("You Win!!!");
+                return;
+            }
+        }
+        public static void Rasstoianie_1(ref int start_i, ref int start_j, int fin_i, int fin_j)   // расстояние между точками равно одному (лежат на одной прямой)
+        {
+
+            int vrem_i = start_j == fin_j ? fin_i : start_i + 2;
+            if (Math.Abs(vrem_i - fin_i) > 2)
+                vrem_i = -4;
+            int vrem_j = start_i == fin_i ? fin_j : start_j + 2;
+            if (Math.Abs(vrem_j - fin_j) > 0)
+                vrem_j -= 4;          // четвертый ход
+            start_i = vrem_i; start_j = vrem_j;             // временные переменные, созданные для сравнения без ошибок   
+            Rasstoianie_2_Vbok(ref start_i, ref start_j, fin_i, fin_j);
+        }
+
+        public static void Rasstoianie_2_Vbok(ref int start_i, ref int start_j, int fin_i, int fin_j)  // точки находятся на одной плоскости на расстоянии = 2
+        {
+            if (start_i == fin_i)
+            {
+                start_i = start_i + 2 > 7 ? start_i - 2 : start_i + 2;
+            }
+            else
+            {
+                start_i = start_i > fin_i ? start_i - 1 : start_i + 1;
+            }
+            if (start_j == fin_j)
+            {
+                start_j = start_j + 2 > 7 ? start_j - 2 : start_j + 2;
+            }
+            else
+            {
+                start_j = start_j > fin_j ? start_j - 1 : start_j + 1;
+            } // был сделан 5 ход, далее идет проверка
+
+            if (Win_or_Loos(start_i, start_j, fin_i, fin_j))  // проверка на правильность
+            {
+                Console.WriteLine("You win!");
+                start_i = fin_i;
+                start_j = fin_j;
+            }
+            else
+                Console.WriteLine("ERROR!");  // для неудачной проверки
+        }
+        public static bool Win_or_Loos(int start_i, int start_j, int fin_i, int fin_j)
+        {
+            return (Math.Abs(start_i - fin_i) + Math.Abs(start_j - fin_j) == 3 && start_i != fin_i && start_j != fin_j);
         }
     }
 }
